1028815916b3336f1379e77cf98d7c4f95fc2cf6
Mark <33554269+lagzar@users.noreply.github.com>
2020-07-26 11:48:42 +0200

    code update

--- jsonRead.php
+++ jsonRead.php
@@ -5,6 +5,6 @@ $data = file_get_contents($url); // put the contents of the file into a variable
 $characters = json_decode($data); // decode the JSON feed
 
 echo $characters[0]->title."<br>";
-echo $characters[0]->street;
+echo $characters[0]->content;
 
 ?>
