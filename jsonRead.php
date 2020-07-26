1ac28c61bc4ef4c10560365c74538c3c6cdc715e
Mark <33554269+lagzar@users.noreply.github.com>
2020-07-26 11:52:39 +0200

    code update

--- jsonRead.php
+++ jsonRead.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = 'src/settings.json'; // path to your JSON file
+$url = 'src/file.json'; // path to your JSON file
 $data = file_get_contents($url); // put the contents of the file into a variable
 $characters = json_decode($data); // decode the JSON feed
 
