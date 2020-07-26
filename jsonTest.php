9c40cfda2a13c273820eb2cf87d4f0ddee8edc3f
Mark <33554269+lagzar@users.noreply.github.com>
2020-07-26 11:47:19 +0200

    Update jsonTest.php

--- jsonTest.php
+++ jsonTest.php
@@ -4,12 +4,12 @@ if (isset($_POST['submit'])) {
     // Creating json file with variables
     $arr = [];
     $inc = 0;
-    $jsonArrayObject = (array('title' => $_POST['title'], 'street' => $_POST['street']));
+    $jsonArrayObject = (array('title' => $_POST['title'], 'content' => $_POST['content']));
     $arr[$inc] = $jsonArrayObject;
     $inc++;
 
     $json_array = json_encode($arr);
-    file_put_contents('src/settings.json', $json_array);
+    file_put_contents('PATH TO JSON FILE', $json_array);
     //echo $json_array;
 }
 ?>
@@ -23,7 +23,7 @@ if (isset($_POST['submit'])) {
 <body>
     <form action="" method="post">
         <input type="text" name="title" placeholder="Enter title">
-        <input type="text" name="street" placeholder="Enter street">
+        <input type="text" name="content" placeholder="Enter content">
         <input type="submit" name="submit">
     </form>
 </body>
